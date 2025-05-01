53eb7c5ce88684b82a6b72ebd56ac1d4e588e2bc
dduga-at-608184981975 <dduga@softserveinc.com>
2025-05-01 08:57:49 +0000

    test

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,4 @@
+# creating main vpc for jenkins and EKS cluster
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   name    = var.vpc_name
@@ -9,6 +10,7 @@ module "vpc" {
   map_public_ip_on_launch = true
 }
 
+# creating SG for Jenkins to open 8080 port
 resource "aws_security_group" "jenkins" {
   name        = "jenkins"
   vpc_id      = module.vpc.vpc_id
@@ -40,6 +42,8 @@ resource "aws_vpc_security_group_egress_rule" "main" {
   ip_protocol       = "-1"
 }
 
+
+# creating role for Jenkins instance to get access for Secret Manager, Parameter Store and SSM connection
 resource "aws_iam_role" "ec2_role" {
   name               = "ec2-role"
   assume_role_policy = jsonencode({
@@ -72,15 +76,92 @@ resource "aws_iam_role_policy_attachment" "attach_ecr" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"
 }
 
-resource "aws_iam_instance_profile" "ec2_ssm_ecr_profile" {
+resource "aws_iam_instance_profile" "ec2_profile" {
   name = "ec2-profile"
   role = aws_iam_role.ec2_role.name
 }
 
-resource "aws_instance" "web" {
+# creating ALB for Jenkins and SG for ALB
+resource "aws_security_group" "alb" {
+  name        = "jenkins-alb-sg"
+  vpc_id      = module.vpc.vpc_id
+
+  tags = {
+    Name = "jenkins-alb-sg"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "lb_main" {
+  security_group_id = aws_security_group.alb.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_egress_rule" "lb_main" {
+  security_group_id = aws_security_group.alb.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
+
+resource "aws_lb" "jenkins" {
+  name               = "jenkins-alb"
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb.id]
+  subnets            = module.vpc.public_subnets
+  enable_deletion_protection = false
+
+  tags = {
+    Name = "jenkins-alb"
+  }
+}
+
+resource "aws_lb_target_group" "jenkins" {
+  name        = "jenkins-tg"
+  port        = 8080
+  protocol    = "HTTP"
+  vpc_id      = module.vpc.vpc_id
+  target_type = "instance"
+
+  health_check {
+    protocol           = "HTTP"
+    path               = "/"
+    matcher            = "200-399"
+    interval           = 30
+    timeout            = 5
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+  }
+
+  tags = {
+    Name = "jenkins-tg"
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.jenkins.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.jenkins.arn
+  }
+}
+
+resource "aws_lb_target_group_attachment" "jenkins" {
+  target_group_arn = aws_lb_target_group.jenkins.arn
+  target_id        = aws_instance.jenkins.id
+  port             = 8080
+}
+
+
+# creating Jenkins instance
+resource "aws_instance" "jenkins" {
   instance_type = "t3.medium"
   ami           = "ami-084568db4383264d4"
-  subnet_id     = module.vpc.public_subnets[0]
+  subnet_id     = module.vpc.private_subnets[0]
   iam_instance_profile = "ec2-profile"
   vpc_security_group_ids = [aws_security_group.jenkins.id]
   user_data     = templatefile("user_data.sh",
@@ -92,8 +173,12 @@ resource "aws_instance" "web" {
   tags = {
     Name = "jenkins"
   }
+  depends_on = [
+    module.vpc
+  ]
 }
 
+# outputs for adding them to ClusterConfig
 output "vpc_id"       { value = module.vpc.vpc_id }
 output "public_subnets"  { value = module.vpc.public_subnets }
 output "private_subnets" { value = module.vpc.private_subnets }
--- infra/user_data.sh
+++ infra/user_data.sh
@@ -1,23 +1,42 @@
 #!/bin/bash
-sudo apt update
-sudo apt install fontconfig openjdk-21-jre -y
+# update and install jenkins
+sudo apt-get update
+sudo apt-get install fontconfig openjdk-21-jre -y
 sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
 echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update
 sudo apt-get install jenkins -y
 sudo systemctl enable jenkins
+
+
+# disable SetubWizard for Jenkins to skip first setup
 sed -i 's/-Djava.net.preferIPv4Stack=true/-Djava.net.preferIPv4Stack=true -Djenkins.install.runSetupWizard=false/g' /etc/default/jenkins
+
+
+# add version to use groovy scripts
 echo "2.0" > /var/lib/jenkins/jenkins.install.UpgradeWizard.state
+
+
+# create directory for user creation script
 mkdir /var/lib/jenkins/init.groovy.d
+
+
+# install awscli
 sudo apt install unzip
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
+
+
+# get script for creating Jenkins user from AWS SSM Parameter Store and add to newly created directory
 aws ssm get-parameter \
     --name "${parameter}" \
     --with-decryption \
     --query "Parameter.Value" \
     --output text > /var/lib/jenkins/init.groovy.d/basic-security.groovy
+
+
+# install docker and add Jenkins user to use docker commands
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 sudo service docker start
@@ -25,14 +44,30 @@ sudo groupadd docker
 sudo usermod -aG docker jenkins
 newgrp docker
 sudo apt install -y jq
+
+
+# restart jenkins to apply new user and new configuration
 sudo systemctl restart jenkins
+
+
+# install jenkins cli for installing plugins
 wget http://localhost:8080/jnlpJars/jenkins-cli.jar
+
+
+# get jenkins credentials srom AWS Secret Manager
 secret_json=$(aws secretsmanager get-secret-value --secret-id ${secret} --query SecretString --output text)
 username=$(echo "$secret_json" | jq -r .username)
 password=$(echo "$secret_json" | jq -r .password)
+
+
+# install plugins
 java -jar jenkins-cli.jar -auth $username:$password -s http://localhost:8080/ install-plugin docker-workflow
 java -jar jenkins-cli.jar -auth $username:$password -s http://localhost:8080/ install-plugin docker-plugin
 java -jar jenkins-cli.jar -auth $username:$password -s http://localhost:8080/ install-plugin pyenv-pipeline
 java -jar jenkins-cli.jar -auth $username:$password -s http://localhost:8080/ install-plugin workflow-aggregator
 java -jar jenkins-cli.jar -auth $username:$password -s http://localhost:8080/ install-plugin git
+java -jar jenkins-cli.jar -auth $username:$password -s http://localhost:8080/ install-plugin github
+
+
+# restart jenkins to apply all changes
 sudo systemctl restart jenkins
